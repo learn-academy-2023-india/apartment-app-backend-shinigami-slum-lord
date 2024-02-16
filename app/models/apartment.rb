69b1433505dc266b47881cd83bf24a21eac09cf5
jwat5623 <jamar34watson56@gmail.com>
2024-02-16 12:08:17 -0800

    adds validations

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,12 +1,4 @@
 class Apartment < ApplicationRecord
   belongs_to :user
-  validates :street, presence: true
-  validates :state, presence: true
-  validates :city, presence: true
-  validates :pets, presence: true
-  validates :unit, presence: true
-  validates :square_footage, presence: true
-  validates :price, presence: true
-  validates :bedrooms, presence: true
-  validates :bathrooms, presence: true
+   validates :street, :state, :city, :pets, :unit, :square_footage, :price, :bedrooms, :bathrooms, :user_id, presence: true
 end
--- spec/models/apartment_spec.rb
+++ spec/models/apartment_spec.rb
@@ -141,4 +141,19 @@ RSpec.describe Apartment, type: :model do
     )
     expect(apartment.errors[:pets]).to include("can't be blank")
   end
+
+  it 'should validate user_id' do
+  apartment = Apartment.create(
+    unit: '745',
+    city: 'Kapolei',
+    state: 'Hawaii',
+    square_footage: 9054,
+    price: 1900,
+    bedrooms: 4,
+    street: '91-2834 Kuakamana',
+    pets: 'hell no',
+    image: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages1.apartments.com%2Fi2%2Fl4d6BYbX57oLW2NKDAIQ7ub5baRQjZFJXif1xLMB94w%2F117%2F502-keawe-st-honolulu-hi-building-photo.jpg%3Fp%3D1&tbnid=GBTyx6jW1IeXgM&vet=12ahUKEwjmpdPXsLCEAxUAh-4BHd-VAQwQMygBegQIARBw..i&imgrefurl=https%3A%2F%2Fwww.apartments.com%2Fhi%2F&docid=qnG_opT7up06tM&w=650&h=433&q=hawaii%20apartments&ved=2ahUKEwjmpdPXsLCEAxUAh-4BHd-VAQwQMygBegQIARBw'
+    )
+    expect(apartment.errors[:user_id]).to include("can't be blank")
+  end
 end
