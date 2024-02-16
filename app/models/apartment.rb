550f4b536f58bbb56a0bdf2ee20aadab2c2cb64e
jwat5623 <jamar34watson56@gmail.com>
2024-02-16 11:48:18 -0800

    adds validations

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,3 +1,12 @@
 class Apartment < ApplicationRecord
   belongs_to :user
+  validates :street, presence: true
+  validates :state, presence: true
+  validates :city, presence: true
+  validates :pets, presence: true
+  validates :unit, presence: true
+  validates :square_footage, presence: true
+  validates :price, presence: true
+  validates :bedrooms, presence: true
+  validates :bathrooms, presence: true
 end
